e13f5e699982d02ed887474bdfba32804c884b41
Alcides QS <alcidesqs@gmail.com>
2022-04-06 10:38:50 -0400

    Se añade archivo MergeSort

---
+++ MergeSort.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class MergeSort implements SortingStrategy {
+
+	@Override
+	public void sort(int[] numbers) {
+		System.out.println("Merge Sort!");
+			// Aqui debe ir codigo de MergeSort
+	}
+}
